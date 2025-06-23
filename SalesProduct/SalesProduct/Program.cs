97e3410bfdac4edcf17e5c896102732c11df85d9
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-06-23 10:57:36 +0600

    Practise OOP

---
+++ SalesProduct/SalesProduct/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SalesProduct;
+
+Sales obj = new Sales();
+obj.productName = "Easy_Shoes";
+obj.productId= "ES123";
+obj.date=DateTime.Now;
+obj.SetPrice(100);
+obj.SetQuantity(8);
+Console.WriteLine("Total Price: " + obj.GetTotalPrice());
+Console.WriteLine("Brand Name : "+ obj.productName+ "\n"+"Brand Id: "+ obj.productId + "\n" +"Purchase Date: "+ obj.date + "\n");
---
+++ SalesProduct/SalesProduct/Sales.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesProduct
+{
+    public class Sales
+    {
+        public string productName;
+        public string productId;
+        public DateTime date;
+        public int price;
+        public int quantity;
+
+        public void SetPrice(int price)
+        {
+            this.price = price;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+
+        public int GetTotalPrice()
+        {
+            return price * quantity;
+        }
+    }
+}
