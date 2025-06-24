8807b63a1d7d63a8aec7e841598f788f28a458b5
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-06-24 17:23:53 +0600

    add Enum

---
+++ Enum/Enum/Enum/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //general Process with out enum
+            int productType = 1;
+            if (productType == 1)
+            {
+                Console.WriteLine($"this is a product type of : {productType} ");
+            }
+            else
+            {
+                Console.WriteLine($"this is a product type of : {productType}");
+            }
+
+
+            // using the enum item from external 
+            if (productType ==(int) EnumExample.regular)
+            {
+                Console.WriteLine($"this is a regular item type of : {productType}");
+            }
+
+           else if (productType == (int)EnumExample.premium)
+            {
+                Console.WriteLine($"this is a regular item type of : {productType}");
+            }
+          else   if (productType == (int)EnumExample.propremium)
+            {
+                Console.WriteLine($"this is a regular item type of : {productType}");
+            }
+
+        }
+    }
+}
