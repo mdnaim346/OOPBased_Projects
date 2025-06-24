c34aa91c0463e62b8f1cda9fa3bd0f8f81090f72
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-06-24 18:14:56 +0600

    sales project added

---
+++ SalesDetails/Sales/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Sales
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sales obj = new Sales("Naim reza", "Nachole", DateTime.Now, 234, 7);
+
+
+            Console.WriteLine("\t\t\t\t\t\t\tThis is a Sales Details\n");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("CustomerName\t\tAddress\t\tDatetime\t\t tprice\t\tquantity\t\ttotalPrice");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine(obj.GetIvoice());
+        }
+    }
+}
---
+++ SalesDetails/Sales/Sales.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales
+{
+    public class Sales
+    {
+        private string customerName;
+        private string Address;
+        private DateTime datetime;
+        private int price;
+        private int quantity;
+
+        public Sales(string customerName, string Address, DateTime datetime, int price, int quantity)
+        {
+            this.customerName = customerName;
+            this.Address = Address;
+            this.datetime = datetime;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public int GetPrice()
+        {
+            return price * quantity;
+        }
+        public string GetIvoice()
+        {
+            string s = "";
+            s += $"{customerName}";
+            s += $"\t\t{Address}";
+            s += $"\t\t{DateTime.Now}";
+            s += $"\t{price}\t";
+            s += $"\t\t{quantity}";
+            s += $"\t\t{GetPrice()}";
+            return s;
+        }
+    }
+}
